807220cbe36162dfe82a3ec01317db2c5964087c
sfblackl <sfblackl@gmail.com>
2015-06-19 15:20:26 -0700

    Attempt 2 on adding custom errors

--- flaskApp/routes.py
+++ flaskApp/routes.py
@@ -2,7 +2,7 @@
 # pylint: disable=unused-import
 
 """Maps all incoming paths to correct functions to handle"""
-from flaskApp import application
+from flaskApp import application, custom_errors
 
 
 @application.route('/tradlabs/v1/health')
@@ -17,3 +17,8 @@ def health():
 def test_500():
     """Forces error to validate error message is correct"""
     print(1/0)
+
+@application.route('/tradlabs/v1/403', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def test_403():
+    """Generates 403 Error.  ToDo: Replace this with valid test"""
+    return custom_errors.error_formatter(code=403)
